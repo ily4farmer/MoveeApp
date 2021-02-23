ea619b270fb1349b2356299f93172744e51eeead
Lexa <ily4.farmer@yandex.ru>
2021-02-23 20:27:27 +0300

    этап обычения завершен, со следующей версии начнется создание проекта movie_app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,61 @@
-function App() {
-  return (
-    <div>
-      <h1>Привет!!!!</h1>
-    </div>
-  );
+import React, { Component } from 'react';
+import Apply from "./components/Apply";
+
+
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    console.log("Конструктор");
+  }
+
+  state = {
+    count: 0
+  }
+
+  plus = () => {
+    let current = this.state.count;
+    current++;
+    this.setState({
+      count: current
+    });
+  }
+
+  minus = () => {
+    let current = this.state.count;
+    current--;
+    if (current <= 0) {
+      current = 0;
+    }
+    this.setState({
+      count: current
+    });
+  }
+
+  componentDidMount() {
+    console.log('Компонент был смонтирован');
+  }
+
+  componentDidUpdate() {
+    console.log('Компонент был изменен');
+  }
+
+  componentWillUnmount() {
+    console.log('Компонент был удален');
+  }
+
+  render() {
+    console.log("Я рендерюсь");
+    const {count} = this.state;
+    const {plus, minus} = this;
+    return(
+      <div>
+        <h1>Текущее число: {count}</h1>
+        <button onClick={minus}>Минус</button>
+        <button onClick={plus}>Плюс</button>
+
+      </div>
+    )
+  }
 }
 
-export default App;
---
+++ src/components/apply.js
@@ -0,0 +1,12 @@
+const Apply = ({name}) => {
+    // const {name} = props;
+    return(
+        <div>
+            <p>Меня зовут {name}</p>
+            <div>Я генерирую HTML код</div>
+            <div>Жизнь должна быть хороша</div>
+        </div>
+    );
+}
+
+export default Apply;
