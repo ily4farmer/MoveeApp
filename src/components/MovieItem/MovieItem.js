df2402810d8bc86620e6a07101bc40322d84cb84
Lexa <ily4.farmer@yandex.ru>
2021-03-10 21:35:06 +0300

    Стили сделанны, поработать над жанрами фильмы

--- src/components/MovieItem/MovieItem.js
+++ src/components/MovieItem/MovieItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import "./MovieItem.css"
 
-export default function MovieItem({id, title, genres,year, summary, poster}) {
+export default function MovieItem({id, title, genres, year, summary, poster}) {
+    console.log(genres);
     return(
         <div className="movie__item">
             <img src={poster} className="movie__img" alt={title} title={title}/> 
@@ -9,9 +10,9 @@ export default function MovieItem({id, title, genres,year, summary, poster}) {
                 <h3 className="movie__title">{title}</h3>
                 <h5 className="movie__year">Год выхода: {year}</h5>
                 <ul className="movie__genres">
-                {genres.map((el, index) => {
-                   return <li key={index} className="movie__genres-item">{el}</li>
-                })}
+                    {genres.map((el, index) => {
+                        return <li key={index} className="movie__genres-item">{el},</li>
+                    })}
                 </ul>
                 <p className="movie__summary">Описание: <br/>{summary.slice(0, 150)}...</p>
             </div>
